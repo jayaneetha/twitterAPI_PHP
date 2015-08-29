ef94e1703a21323f02a5a6cfa7b9423eddf377ea
Thejan Rajapakshe <coder.clix@gmail.com>
2015-08-29 14:33:31 +0530

    init

--- index.php
+++ index.php
@@ -1,24 +1,6 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
 
-require 'setting.php';
-
-$twitter = new TwitterAPIExchange($settings);
-
-if (isset($_GET['max_id'])) {
-    $getfield = '?q=' . $_GET['q'] . '&count=30&max_id=' . $_GET['max_id'];
-}else{
-    $getfield = '?q=' . $_GET['q'] . '&count=30';
-}
-
-$url = 'https://api.twitter.com/1.1/search/tweets.json';
-$requestMethod = 'GET';
-
-$response = $twitter->setGetfield($getfield)
-    ->buildOauth($url, $requestMethod)
-    ->performRequest();
-header('Content-Type: application/json');
-echo $response;
+echo "<h1>Welcome to Twitter API </h1>";
 
 
 
