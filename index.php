7c9812e2bcddcba97c6537ec89088b2d6bd8bf4f
Thejan Rajapakshe <coder.clix@gmail.com>
2015-08-30 21:07:26 +0530

    Init

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require 'TwitterAPIExchange.php';
+require __DIR__ . '/vendor/autoload.php';
 
 require 'setting.php';
 
--- setting.php
+++ setting.php
@@ -1,7 +1,7 @@
 <?php
 $settings = array(
-    'oauth_access_token' => "218770908-0AQ58t6tOCnWZwhckq7bTEfvYC2LNcJ3Q7oA2Izc",
-    'oauth_access_token_secret' => "lIevpljqVHzreqkhNOm8glDBAtfU7EU1O6d5e4BY40zge",
-    'consumer_key' => "LOjvikDlpuXrdAmvupqTPFK9N",
-    'consumer_secret' => "4qt8y050APlFZ9CgEmGXWCo1prBWtPMdhD3nlRX2mMmHq98Lfb"
+    'oauth_access_token' => "",
+    'oauth_access_token_secret' => "",
+    'consumer_key' => "",
+    'consumer_secret' => ""
 );
